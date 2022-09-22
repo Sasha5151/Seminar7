91c31df42d77ee991be86a616d498a86fbcd096d
Sasha <F712@rambler.ru>
2022-09-22 10:54:22 +0300

    Спираль

--- Program.cs
+++ Program.cs
@@ -201,14 +201,15 @@
 
 //  // End   
 
-// Задача . Вывести  элементов нах на периметре матрици .
+// Задача . Улитка .
 
 int[,] array = new int[,] {
        { 1, 4, 7, 2} ,
        { 5, 9, 2, 3} ,
        { 8, 4, 2, 4} ,
        { 8, 4, 2, 4} ,
-       { 8, 4, 2, 4}
+       { 8, 4, 2, 4} ,
+       { 3, 5, 6, 1}
 };
 PrintArray( array) ;
 
@@ -216,26 +217,29 @@ void PrintArray(int[,] array)
 {
     int rows = array.GetLength(0);
     int columns = array.GetLength(1);
+    int limit = (Math.Max( rows , columns) % 2 == 0 ) ? Math.Max( rows , columns) / 2 :
+        Math.Max( rows , columns) / 2 + 1 ;
     
-    for (int k = 0; k < length; k++)
+    for (int k = 0; k < limit ; k++)
     {
-        
-    
-        for (int j = 0; j < columns; j++)
+        for (int j = k; j < columns - k ; j++)
         {
-            System.Console.Write(array[0 , j] + " "); 
+            System.Console.Write(array[k , j] + " "); 
         }
-        for (int i = 1; i < rows; i++)
+        for (int i = k+1; i < rows - k ; i++)
         {
-            System.Console.Write(array[i , columns - 1] + " "); 
+            System.Console.Write(array[i , columns - k - 1] + " "); 
         }
-        for (int j = columns-2 ; j >= 0; j--)
+
+        var z = (k == 0) ? k : k + 1;
+
+        for (int j = columns-k-2 ; j >= z; j--)
         {
-            System.Console.Write(array[rows-1 , j] + " "); 
+            System.Console.Write(array[rows - 1 - k , j] + " "); 
         }
-        for (int i =rows - 2; i > 0; i--)
+        for (int i =rows - k-2; i > k; i--)
         {
-            System.Console.Write(array[i , 0] + " "); 
+            System.Console.Write(array[i , k] + " "); 
         }
     }
 }
